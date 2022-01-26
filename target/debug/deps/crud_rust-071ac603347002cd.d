a066aa58ba1017a44186c8022a440082e7e05aa5
Darshan V <darshanv3v@gmail.com>
2022-01-26 19:46:55 +0530

    initial setup

---
+++ target/debug/deps/crud_rust-071ac603347002cd.d
@@ -0,0 +1,5 @@
+c:\DFiles\rust_projects\crud_rust\target\debug\deps\crud_rust-071ac603347002cd.rmeta: src\main.rs
+
+c:\DFiles\rust_projects\crud_rust\target\debug\deps\crud_rust-071ac603347002cd.d: src\main.rs
+
+src\main.rs:
---
+++ target/debug/deps/crud_rust-40c6c465f8e88aea.d
@@ -0,0 +1,5 @@
+c:\DFiles\rust_projects\crud_rust\target\debug\deps\crud_rust-40c6c465f8e88aea.rmeta: src\main.rs
+
+c:\DFiles\rust_projects\crud_rust\target\debug\deps\crud_rust-40c6c465f8e88aea.d: src\main.rs
+
+src\main.rs:
